096a4ece79cf077f771620bdab93fc777e6c3a25
Chu yukang <2867908314@qq.com>
2020-04-05 21:40:10 +0800

    CutRodSolver bug fix

--- algorithms/DynamicProgramming/cutRod/test.cpp
+++ algorithms/DynamicProgramming/cutRod/test.cpp
@@ -6,9 +6,9 @@ extern int getMaxValue(int p[],int n,int length);
 
 int main()
 {
-    int array[10]={1,5,8,9,10,17,17,20,24,30};
+    int array[11]={0,1,5,8,9,10,17,17,20,24,30};
     int length = 5;
     cout << "The Max Value could get from rod(" << length <<") is " 
-        << getMaxValue(array,10,length) << "\n";
+        << getMaxValue(array,11,length) << "\n";
     return 0;
 }
