91e197930c1df24da70cc13a2409222ff3a7b0f8
Chu yukang <2867908314@qq.com>
2020-04-12 12:07:33 +0800

    增加注释&&修改样例

--- algorithms/Graph/BellmanFord.cpp
+++ algorithms/Graph/BellmanFord.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include<vector>
 #include"../../graph/Graph.h"
 
@@ -7,19 +8,21 @@ vector<int> BellmanFord(Graph& g,int s=0)
 {
     int v_num = g.getVnum();
     vector<int> dis(v_num);
+    //初始不存在从源点到各个顶点的路径
     for(int i=0;i<v_num;i++)
         dis[i] = __INT_MAX__;//trick
 
     dis[s] = 0;
-    //最多遍历V次
-    for(int i=0;i<v_num;i++)
+    //最多遍历V-1次
+    for(int i=1;i<v_num;i++)
     {
         //简陋的遍历边
         for(int u=0;u<v_num;u++)
         {
             for(int v=0;v<v_num;v++)
             {
-                if(g.existsEdge(u,v))
+                //u,v边存在 且 存在从s到u的路径
+                if(g.existsEdge(u,v)&&dis[u]<__INT_MAX__)
                 {
                     int temp=dis[u]+g.getWeight(u,v);
                     if(dis[v]>temp)//松弛步
--- algorithms/Graph/SingleSourceShortestPath.cpp
+++ algorithms/Graph/SingleSourceShortestPath.cpp
@@ -9,19 +9,30 @@ using namespace std;
 
 extern vector<int> Dijkstra(Graph& g,int s=0);
 extern vector<int> BellmanFord(Graph& g,int s=0);
+extern bool BellmanFord_N(Graph& g,int s,vector<int>& dis);
 
 int main()
 {
     Graph g(5);
-    g.insertEdge(0,1,10);
-    g.insertEdge(0,2,3);
-    g.insertEdge(1,2,1);
-    g.insertEdge(1,3,2);
-    g.insertEdge(2,1,4);
-    g.insertEdge(2,3,8);
-    g.insertEdge(2,4,2);
-    g.insertEdge(3,4,7);
-    g.insertEdge(4,3,9);
+    g.insertEdge(1,2,5);
+    g.insertEdge(1,3,8);
+    g.insertEdge(1,4,-4);
+    g.insertEdge(3,2,-3);
+    g.insertEdge(3,4,9);
+    g.insertEdge(4,2,4);
+    g.insertEdge(4,0,2);
+    g.insertEdge(0,1,6);
+    g.insertEdge(0,3,7);
+    g.insertEdge(2,1,-2);
+//    g.insertEdge(0,1,10);
+//    g.insertEdge(0,2,3);
+//    g.insertEdge(1,2,1);
+//    g.insertEdge(1,3,2);
+//    g.insertEdge(2,1,4);
+//    g.insertEdge(2,3,8);
+//    g.insertEdge(2,4,2);
+//    g.insertEdge(3,4,7);
+//    g.insertEdge(4,3,9);
 
     cout << "Dijkstra Algorithm:\n";
     vector<int> dis1 = Dijkstra(g,0);
